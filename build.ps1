bbffb32c4a648c3312846490bea3f4645cf74ff0
JC Ryan <jc_ryan@live.com>
2025-04-10 10:35:40 -0400

    Changed load order but still fails in Core

--- build.ps1
+++ build.ps1
@@ -3,12 +3,12 @@ param(
     [string[]]$Task = 'ModuleBuild'
 )
 
-$DependentModules = @('PSDeploy','InvokeBuild','PlatyPS') # add pester when pester tests are added
+$DependentModules = @('PlatyPS','PSDeploy','InvokeBuild') # add pester when pester tests are added
 Foreach ($Module in $DependentModules){
     If (-not (Get-Module $module -ListAvailable)){
         Install-Module -name $Module -Scope CurrentUser -Force
     }
-    Import-Module $module -ErrorAction Stop
+    Import-Module $module -ErrorAction Stop 
 }
 if ((Get-Module pester -ListAvailable | Sort-Object version | Select-Object -Last 1).Version -lt 4.6){
     Install-Module 'Pester' -MinimumVersion 4.6 -Scope CurrentUser -Force
