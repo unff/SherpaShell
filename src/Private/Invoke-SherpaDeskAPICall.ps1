8f51e1635c715c7b04d04454d8a1946d0eaaf05b
JC Ryan <jc_ryan@live.com>
2025-04-10 15:34:09 -0400

    removed debug messages

--- src/Private/Invoke-SherpaDeskAPICall.ps1
+++ src/Private/Invoke-SherpaDeskAPICall.ps1
@@ -18,13 +18,12 @@ Function Invoke-SherpaDeskAPICall {
     }
     
     If($Method -eq 'Get') {
-        # Start a timer to keep track of calls per second
-        $timer = [Diagnostics.Stopwatch]::StartNew()
+        # Start a Timer to keep track of calls per second
+        $Timer = [Diagnostics.Stopwatch]::StartNew()
         # Get Page zero
-        # $resp = Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header
-        $resp = Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header -ContentType 'application/json' -Body $Body
-        # Check if the response has fewer than 25 items, indicating no more pages
-        If ($resp.Count -lt 25){
+        $response = Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header -ContentType 'application/json' -Body $Body
+        # Check if a single page was requested, or the response has fewer than 25 items, indicating no more pages
+        If ($Resource.Contains('?page=') -Or $response.Count -lt 25){
             $hasMoreResults = $false # If the response is less than 25 items, there are no more pages
         } else {
             $hasMoreResults = $true # If the response is 25 items, there are more pages to fetch
@@ -32,12 +31,12 @@ Function Invoke-SherpaDeskAPICall {
         $page = 1   # Set page counter to 1 for the next page
         
         while ($hasMoreResults) {
-            Write-Information "total seconds elapsed: $($timer.elapsed.totalseconds) retrieving page ${page}" -InformationAction Continue
+            Write-Information "total seconds elapsed: $($Timer.elapsed.totalseconds) retrieving page ${page}" -InformationAction Continue
             # Invoke the API call for the next page of results
             # The page number is passed as a query parameter to the API call
             $currentPageResults = Invoke-RestMethod -Method $Method -Uri "$baseUri/$resource/?page=$page" -Headers $header
-            # Flatten the results by adding each object from the current page to $resp
-            $currentPageResults | ForEach-Object { $resp += $_ }
+            # Flatten the results by adding each object from the current page to $response
+            $currentPageResults | ForEach-Object { $response += $_ }
         
             # Check if the current page has fewer than 25 objects, indicating no more pages
             if ($currentPageResults.Count -lt 25) {
@@ -46,10 +45,10 @@ Function Invoke-SherpaDeskAPICall {
                 $page++ # Increment the page counter to fetch the next page
             }
         }
-        Write-Information "finished retrieval. total seconds elapsed: $($timer.elapsed.totalseconds)" -InformationAction Continue
-        $timer.Stop() # Stop the timer
-        # $resp now contains a single array of objects from all pages
-        return $resp
+        Write-Information "finished retrieval. total seconds elapsed: $($Timer.elapsed.totalseconds)" -InformationAction Continue
+        $Timer.Stop() # Stop the Timer
+        # $response now contains a single array of objects from all pages
+        return $response
 
     }
     ElseIf(@('Post','Put','Delete') -contains $Method) {
