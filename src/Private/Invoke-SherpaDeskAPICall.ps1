e33d64f7080cbaf67187ac0cb6a3d612f96aab90
JC Ryan <jc_ryan@live.com>
2025-04-10 10:36:39 -0400

    Added pagination and GET body support

--- src/Private/Invoke-SherpaDeskAPICall.ps1
+++ src/Private/Invoke-SherpaDeskAPICall.ps1
@@ -17,9 +17,42 @@ Function Invoke-SherpaDeskAPICall {
         Accept = 'application/json'
     }
     
-    If($Method -eq 'Get'){
-        Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header
-    }ElseIf(@('Post','Put') -contains $Method){
+    If($Method -eq 'Get') {
+        # Start a timer to keep track of calls per second
+        $timer = [Diagnostics.Stopwatch]::StartNew()
+        # Get Page zero
+        # $resp = Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header
+        $resp = Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header -ContentType 'application/json' -Body $Body
+        # Check if the response has fewer than 25 items, indicating no more pages
+        If ($resp.Count -lt 25){
+            $hasMoreResults = $false # If the response is less than 25 items, there are no more pages
+        } else {
+            $hasMoreResults = $true # If the response is 25 items, there are more pages to fetch
+        }
+        $page = 1   # Set page counter to 1 for the next page
+        
+        while ($hasMoreResults) {
+            Write-Information "total seconds elapsed: $($timer.elapsed.totalseconds) retrieving page ${page}" -InformationAction Continue
+            # Invoke the API call for the next page of results
+            # The page number is passed as a query parameter to the API call
+            $currentPageResults = Invoke-RestMethod -Method $Method -Uri "$baseUri/$resource/?page=$page" -Headers $header
+            # Flatten the results by adding each object from the current page to $resp
+            $currentPageResults | ForEach-Object { $resp += $_ }
+        
+            # Check if the current page has fewer than 25 objects, indicating no more pages
+            if ($currentPageResults.Count -lt 25) {
+                $hasMoreResults = $false
+            } else {
+                $page++ # Increment the page counter to fetch the next page
+            }
+        }
+        Write-Information "finished retrieval. total seconds elapsed: $($timer.elapsed.totalseconds)" -InformationAction Continue
+        $timer.Stop() # Stop the timer
+        # $resp now contains a single array of objects from all pages
+        return $resp
+
+    }
+    ElseIf(@('Post','Put','Delete') -contains $Method) {
         Invoke-RestMethod -Method $Method -Uri "$baseUri/$Resource" -Headers $header -ContentType 'application/json' -Body $Body
     }
 }
