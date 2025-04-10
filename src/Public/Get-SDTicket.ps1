fa7fefa2575ce87985dd2de90bc77965f3926df9
JC Ryan <jc_ryan@live.com>
2025-04-10 10:39:17 -0400

    Added available queries

--- src/Public/Get-SDTicket.ps1
+++ src/Public/Get-SDTicket.ps1
@@ -1,18 +1,27 @@
 Function Get-SDTicket{
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'ByKey')]
     Param(
-        [parameter(
-            ParameterSetName = 'ByKey'
-        )]
-        [string]$Key,
+        [parameter(ParameterSetName = 'ByKey')] [string]$Key,
+        [parameter(ParameterSetName = 'ByPage')] [int]$Page,
+        [parameter(ParameterSetName = 'ByStatus')] [string]$Status,
+        [parameter(ParameterSetName = 'BySearch')] [string]$Search,
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
         [string]$ApiKey = $authConfig.ApiKey
     )
+
+    # Validate the key parameter if provided
     $resource = 'tickets'
     If($PSCmdlet.ParameterSetName -eq 'ByKey'){
         $resource = "$resource/$key"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByPage') {
+        $resource = "${resource}?page=${Page}"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByStatus') {
+        $resource = "${resource}?status=${Status}"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'BySearch') {
+        $resource = "${resource}?search=${Search}"
     }
 
-    Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+     Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+
 }
