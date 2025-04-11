78e0d509fb31e8384e181ea8028aaa6b50fbbe00
JC Ryan <jc_ryan@live.com>
2025-04-11 15:28:14 -0400

    update

--- src/Public/Get-SDProject.ps1
+++ src/Public/Get-SDProject.ps1
@@ -1,18 +1,26 @@
 Function Get-SDProject {
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'All')]
     Param(
-        [parameter(
-            ParameterSetName = 'ByKey'
-        )]
-        [string]$Key,
+        [parameter(ParameterSetName = 'ByAccount')] [string]$AccountID,
+        [parameter(ParameterSetName = 'ByTech')] [string]$TechID,
+        [parameter(ParameterSetName = 'HasStatistics')] [switch]$HasStats,
+        [parameter(ParameterSetName = 'ByStatusID')] [string]$StatusID,
+        [parameter(ParameterSetName = 'All')] [switch]$All,
+
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
         [string]$ApiKey = $authConfig.ApiKey
     )
     $resource = 'projects'
-    If($PSCmdlet.ParameterSetName -eq 'ByKey'){
-        $resource = "$resource/$key"
-    }
+    If($PSCmdlet.ParameterSetName -eq 'ByAccount'){
+        $resource = "${resource}?account=${AccountID}"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByTech') {
+        $resource = "${resource}?tech=${TechID}"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'HasStatistics') {
+        $resource = "${resource}?is_with_statistics=true"
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByStatusID') {
+        $resource = "${resource}?active_status=${StatusID}"
+    } 
 
     Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
 }
--- src/Public/Get-SDTicket.ps1
+++ src/Public/Get-SDTicket.ps1
@@ -1,16 +1,21 @@
 Function Get-SDTicket{
-    [cmdletbinding(DefaultParameterSetName = 'ByKey')]
+    [cmdletbinding(DefaultParameterSetName = 'All')]
     Param(
         [parameter(ParameterSetName = 'ByKey')] [string]$Key,
         [parameter(ParameterSetName = 'ByPage')] [int]$Page,
         [parameter(ParameterSetName = 'ByStatus')] [string]$Status,
+        [parameter(ParameterSetName = 'ByStatus')] [string]$Role,
+        [parameter(ParameterSetName = 'ByDateRange')] [string]$StartDate,
+        [parameter(ParameterSetName = 'ByDateRange')] [string]$EndDate,
         [parameter(ParameterSetName = 'BySearch')] [string]$Search,
+        [parameter(ParameterSetName = 'All')] [switch]$All, # Get all tickets. Value is ignored. This is the default if no other params are sent.
+
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
         [string]$ApiKey = $authConfig.ApiKey
     )
 
-    # Validate the key parameter if provided
+    # TODO: Validate the parameters if provided
     $resource = 'tickets'
     If($PSCmdlet.ParameterSetName -eq 'ByKey'){
         $resource = "$resource/$key"
@@ -18,6 +23,17 @@ Function Get-SDTicket{
         $resource = "${resource}?page=${Page}"
     } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByStatus') {
         $resource = "${resource}?status=${Status}"
+        If ($Role) {
+            $resource = "${resource}&role=${Role}"
+        }
+    } ElseIf ($PSCmdlet.ParameterSetName -eq 'ByDateRange') {
+        If ($StartDate -And $EndDate) {
+            $resource = "${resource}?start_date=${StartDate}&end_date=${EndDate}"
+        } ElseIf ($StartDate) {
+            $resource = "${resource}?start_date=${StartDate}"
+        } ElseIf ($EndDate) {
+            $resource = "${resource}?end_date=${EndDate}"
+        }
     } ElseIf ($PSCmdlet.ParameterSetName -eq 'BySearch') {
         $resource = "${resource}?search=${Search}"
     }
