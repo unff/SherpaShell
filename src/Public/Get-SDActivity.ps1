6db43304fe3807c6aec4942de5cf9f7f6504d02e
JC Ryan <jc_ryan@live.com>
2025-04-14 11:19:38 -0400

    variable cleanup

--- src/Public/Get-SDActivity.ps1
+++ src/Public/Get-SDActivity.ps1
@@ -1,7 +1,9 @@
 Function Get-SDActivity {
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'All')]
     Param(
         [parameter(ParameterSetName = 'ByUserID')] [string]$UserID,
+        [parameter(ParameterSetName = 'All')] [switch]$All, # Get all activity. Value is ignored. This is the default if no other params are sent.
+
 
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
@@ -9,7 +11,7 @@ Function Get-SDActivity {
     )
     $resource = 'activity'
     If($PSCmdlet.ParameterSetName -eq 'ByUserID'){
-        $resource = "${resource}?user=${key}"
+        $resource = "${resource}?user=${UserID}"
     }
 
     Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
